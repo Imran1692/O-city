3dd446b6884dc9b3dc92624c15f3d5727279e644
Imran1692 <imranhossain386434@gmail.com>
2025-12-22 13:32:19 +0600

    media and menu design

--- o-city/src/components/Hero.jsx
+++ o-city/src/components/Hero.jsx
@@ -3,13 +3,13 @@ import fruites from '../images/fruites.png'
 import Button from './Button'
 const Hero = () => {
     return (
-        <section className='mx-10 my-3.5 flex justify-center items-center py-15'>
+        <section className='mx-10 my-3.5 flex justify-center items-center md:py-15  py-20 md:flex-row flex-col'>
 
             {/* hero contain */}
             <div className='flex-1'>
-                <span className='rounded-2xl  bg-amber-100 text-amber-500 text-xl'>Export Best Quality... </span>
+                <span className='rounded-2xl  bg-amber-100 text-amber-500   text-lg md:text-xl'>Export Best Quality... </span>
 
-                <h1 className="text-6xl font-extrabold leading-tight">
+                <h1 className="text-5xl font-semibold leading-normal    md:text-6xl font-extrabold leading-tight">
                     Tasty Organic{" "} <br />
                     <span className="bg-gradient-to-r from-amber-500 to-orange-600 bg-clip-text text-transparent">
                         Fruits
@@ -21,7 +21,7 @@ const Hero = () => {
                     In Your City
                 </h1>
 
-                <p className='text-gray-500 font-light font-semibold text-2xl'>
+                <p className='md:text-gray-500 font-light font-semibold text-2xl font-medium text-sm'>
                     Bred for a high content of beneficial substances.Our products are all fresh and healthy
                 </p>
                 <br />
--- o-city/src/components/Navber.jsx
+++ o-city/src/components/Navber.jsx
@@ -1,10 +1,17 @@
 import { FaHeart } from "react-icons/fa6";
 import { TiShoppingCart } from "react-icons/ti";
 import { IoSearch } from "react-icons/io5";
-function Navber(){
+import { IoMenu } from "react-icons/io5";
+import { RiMenu4Fill } from "react-icons/ri";
+import { useState } from "react";
+function Navber() {
+    const [showMenu,setShowMenu] = useState(false);
+    const menubar = () => {
+        setShowMenu(!showMenu);
+    }
     return (
-        <header className=" mx-10 my-3.5 fixed top-0 left-0 right-0">
-            <nav className="px-2 py-2 mx-6  flex justify-between items-center ">
+        <header className=" mx-10 my-3.5 fixed top-0 left-0 right-0 ">
+            <nav className="md:h-[12vh] h-[10vh] px-2 py-2 mx-6  flex justify-between items-center ">
                 {/* Logo */}
                 <a href="#" className="text-3xl font-bold">
                     <span className="text-amber-400">O</span>
@@ -14,7 +21,7 @@ function Navber(){
 
                 {/* Desktop Menu */}
 
-                <ul className="flex items-center gap-x-15 ">
+                <ul className="md:flex items-center gap-x-15 hidden ">
                     <li>
                         <a href="#" className="font-semibold  text-amber-500 tracking-wider ">Home </a>
                     </li>
@@ -28,12 +35,12 @@ function Navber(){
                         <a href="#" className="font-semibold  tracking-wider hover:text-amber-500">Contact Us </a>
                     </li>
                 </ul>
-                
+
                 {/* Nav serch */}
-                <div className="flex items-center gap-2 border border-zinc-300 rounded-full px-3 h-11 
-                focus-within:ring-2 focus-within:ring-amber-400 transition">
-                    <input type="text" placeholder="input what you went" className="focus:outline-none px-1.5 h-9 "/>
-                    <button className=" bg-amber-400 hover: bg-gradient-to-b from-amber-400 to-amber-600 transition h-10 w-10 flex items-center justify-center rounded-full text-xl">
+                <div className="md:flex items-center gap-2 border border-zinc-300 rounded-full px-3 h-11 
+                focus-within:ring-2 focus-within:ring-amber-400 transition hidden">
+                    <input type="text" placeholder="input what you went" className="focus:outline-none px-1.5 h-9 " />
+                    <button className=" bg-amber-400 hover:bg-gradient-to-b from-amber-400 to-amber-600  transition h-10 w-10 flex items-center justify-center rounded-full text-xl">
                         <IoSearch />
                     </button>
                 </div>
@@ -46,7 +53,34 @@ function Navber(){
                     <a href="#" className="text-2xl">
                         <TiShoppingCart />
                     </a>
+                    {/* menubar */}
+                    <a href="#" className="text-2xl" onClick={menubar}>
+                       { showMenu ? <RiMenu4Fill />:<IoMenu />}
+                    </a>
                 </div>
+                {/* Mobile design */}
+                <ul className={`flex flex-col items-center  gap-y-15 gap-x-15 md:hidden bg-amber-500/15 backdrop-blur-sm backdrop-blur-2xl rounded-2xl p-10 absolute top-30 -left-full transform -translate-x-1/2 transition-all duration-500 ${showMenu ? 'left-1/2': " "}`}>
+                    <li>
+                        <a href="#" className="font-semibold  text-amber-500 tracking-wider ">Home </a>
+                    </li>
+                    <li>
+                        <a href="#" className="font-semibold  tracking-wider hover:text-amber-500">About Us </a>
+                    </li>
+                    <li>
+                        <a href="#" className="font-semibold  tracking-wider hover:text-amber-500">Process </a>
+                    </li>
+                    <li>
+                        <a href="#" className="font-semibold  tracking-wider hover:text-amber-500">Contact Us </a>
+                    </li>
+                    <li 
+                      className="flex     items-center gap-2 border    border-zinc-300 rounded-full  px-3 h-11 
+                     focus-within:ring-2 focus-within:ring-amber-400  transition ">
+                        <input type="text" placeholder="input what you went" className="focus:outline-none px-1.5 h-9 " />
+                        <button className=" bg-amber-400 hover: bg-gradient-to-b from-amber-400 to-amber-600 transition h-10 w-10 flex items-center justify-center rounded-full text-xl">
+                            <IoSearch />
+                        </button>
+                    </li>
+                </ul>
 
             </nav>
         </header>
